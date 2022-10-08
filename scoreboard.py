551a3b79a696530ec8cc0ca4c5f8266f0a1f8737
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-08 13:40:24 +0100

    Hability to save the game

--- scoreboard.py
+++ scoreboard.py
@@ -8,23 +8,31 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.read_file()
         self.penup()
         self.color("white")
         self.hideturtle()
-        self.goto(0,270)
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def read_file(self):
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
+    def write_file(self):
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_file()
+        self.score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
         self.write(arg=f'Score: {self.score}  High Score: {self.high_score}', align=ALIGMENT, font=FONT)
-
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-        self.score = 0
-        self.update_scoreboard()
-
 
     def score_point(self):
         self.score += 1
